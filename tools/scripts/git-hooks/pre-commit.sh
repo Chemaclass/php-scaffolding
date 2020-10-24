5572f7fa9731320098c29295f14a038ae6e77768
Chemaclass <chemaclass@outlook.es>
2020-10-24 16:52:16 +0200

    introduce composer csrun command

--- tools/scripts/git-hooks/pre-commit.sh
+++ tools/scripts/git-hooks/pre-commit.sh
@@ -3,7 +3,7 @@
 set -e
 
 if docker ps | grep -q php_scaffolding; then
-    docker-compose exec -T php_scaffolding composer csfix
+    docker-compose exec -T php_scaffolding composer csrun
     docker-compose exec -T php_scaffolding composer test-unit
 else
     echo "Are you sure Docker is running?"
