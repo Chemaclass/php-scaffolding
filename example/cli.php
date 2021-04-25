7fb824ddbd229d714efe28b9893a9bfd6d1824f0
Chemaclass <chemaclass@outlook.es>
2021-04-25 16:41:34 +0200

    Update gacela dependency

--- example/cli.php
+++ example/cli.php
@@ -4,11 +4,8 @@
 declare(strict_types=1);
 
 use App\ExampleModule\Facade;
-use Gacela\Framework\Config;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
-
-Config::setApplicationRootDir(getcwd());
 
 $facade = new Facade();
 $sum = $facade->add(1, 2, 3);
